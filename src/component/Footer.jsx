2741ba14d8eb138a27d31958816b4ef66aa06caf
parkjunhyun <jhyun970405@gmail.com>
2024-04-02 11:23:24 +0900

    pc 와이어

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-full flex justify-center'>
+        <div className='w-full h-10 flex justify-center items-center bg-black'>
+            <div className=''>
+                <p className='text-white'>발바닥</p>
+            </div>        
+        </div>
+    </div>
+  )
+}
