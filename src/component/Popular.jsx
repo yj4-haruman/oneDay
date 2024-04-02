a099b2fa925b606076b84d47cbae3c976f2540f5
ldh <ai04120@naver.com>
2024-04-02 12:55:03 +0900

    1

--- src/component/Popular.jsx
+++ src/component/Popular.jsx
@@ -1,122 +1,155 @@
-import React from 'react'
-import world from '../img/world.png';
-import user from '../img/user.png';
-import star from '../img/star.png';
-
+import React from "react";
+import world from "../img/world.png";
+import user from "../img/user.png";
+import star from "../img/star.png";
 
 export default function Popular() {
-  return (
-   <>
-    <div className='w-full flex px-5 my-12 '>
-      <div className='w-full h-full '>
-        {/* title */}
-        <div className='w-full flex flex-col gap-y-2'>
-            {/* title */}
-            <div>
-              <p className='font-black text-[35px]'>인기 클래스</p>
+    return (
+        <>
+            <div className="w-full flex px-5 my-12 ">
+                <div className="w-full h-full ">
+                    {/* title */}
+                    <div className="w-full flex flex-col gap-y-2">
+                        {/* title */}
+                        <h2 className="font-black text-[35px]">인기 클래스</h2>
+                        {/* title des */}
+                        <div>
+                            <p className="font-semibold text-[20px]">어떤 클래스가 좋을지 모르겠다면? 많은 사람이 찾는 클래스부터 시작해보세요.</p>
+                        </div>
+                    </div>
+                    {/* one-class */}
+                    <div className="w-full flex mt-10 text-center gap-x-10 justify-center ">
+                        <div className="w-full sm:w-[345px] h-full shadow-lg bg-white flex flex-col gap-y-[12px] rounded-2xl mb-3">
+                            {/* class img */}
+                            <div className="w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden">
+                                <img src="" alt="" />
+                                이미지라오
+                            </div>
+                            {/* class genre */}
+                            <div className="w-full flex justify-between px-2 items-center">
+                                <div className=" border border-black p-1 rounded-xl">클래스 종류</div>
+                                <div className="flex gap-x-5">
+                                    <div className=" flex gap-x-1 items-center">
+                                        <img className="w-[16px] h-[18px]" src={world} alt="World" />
+                                        온라인
+                                    </div>
+                                    <div className="flex gap-x-1 items-center">
+                                        <img className="w-[18px] h-[16px]" src={user} alt="" />5 ~ 10명
+                                    </div>
+                                </div>
+                            </div>
+                            {/* class name */}
+                            <div className="w-full flex px-2">
+                                <p>클래스 이름 : Lorem ipsum dolor sit</p>
+                            </div>
+                            {/* class price */}
+                            <div className="flex justify-between px-2">
+                                <div className="flex gap-x-2 items-center">
+                                    <img src={star} alt="" />
+                                    찜하기
+                                </div>
+                                <div>100,000원</div>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-[345px] h-full shadow-lg bg-white hidden sm:flex flex-col gap-y-[12px] rounded-2xl mb-3">
+                            {/* class img */}
+                            <div className="w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden">
+                                <img src="" alt="" />
+                                이미지라오
+                            </div>
+                            {/* class genre */}
+                            <div className="w-full flex justify-between px-2 items-center">
+                                <div className=" border border-black p-1 rounded-xl">클래스 종류</div>
+                                <div className="flex gap-x-5">
+                                    <div className=" flex gap-x-1 items-center">
+                                        <img className="w-[16px] h-[18px]" src={world} alt="World" />
+                                        온라인
+                                    </div>
+                                    <div className="flex gap-x-1 items-center">
+                                        <img className="w-[18px] h-[16px]" src={user} alt="" />5 ~ 10명
+                                    </div>
+                                </div>
+                            </div>
+                            {/* class name */}
+                            <div className="w-full flex px-2">
+                                <p>클래스 이름 : Lorem ipsum dolor sit</p>
+                            </div>
+                            {/* class price */}
+                            <div className="flex justify-between px-2">
+                                <div className="flex gap-x-2 items-center">
+                                    <img src={star} alt="" />
+                                    찜하기
+                                </div>
+                                <div>100,000원</div>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-[345px] h-full shadow-lg bg-white hidden lg:flex flex-col gap-y-[12px] rounded-2xl mb-3">
+                            {/* class img */}
+                            <div className="w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden">
+                                <img src="" alt="" />
+                                이미지라오
+                            </div>
+                            {/* class genre */}
+                            <div className="w-full flex justify-between px-2 items-center">
+                                <div className=" border border-black p-1 rounded-xl">클래스 종류</div>
+                                <div className="flex gap-x-5">
+                                    <div className=" flex gap-x-1 items-center">
+                                        <img className="w-[16px] h-[18px]" src={world} alt="World" />
+                                        온라인
+                                    </div>
+                                    <div className="flex gap-x-1 items-center">
+                                        <img className="w-[18px] h-[16px]" src={user} alt="" />5 ~ 10명
+                                    </div>
+                                </div>
+                            </div>
+                            {/* class name */}
+                            <div className="w-full flex px-2">
+                                <p>클래스 이름 : Lorem ipsum dolor sit</p>
+                            </div>
+                            {/* class price */}
+                            <div className="flex justify-between px-2">
+                                <div className="flex gap-x-2 items-center">
+                                    <img src={star} alt="" />
+                                    찜하기
+                                </div>
+                                <div>100,000원</div>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-[345px] h-full shadow-lg bg-white hidden xl:flex flex-col gap-y-[12px] rounded-2xl mb-3">
+                            {/* class img */}
+                            <div className="w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden">
+                                <img src="" alt="" />
+                                이미지라오
+                            </div>
+                            {/* class genre */}
+                            <div className="w-full flex justify-between px-2 items-center">
+                                <div className=" border border-black p-1 rounded-xl">클래스 종류</div>
+                                <div className="flex gap-x-5">
+                                    <div className=" flex gap-x-1 items-center">
+                                        <img className="w-[16px] h-[18px]" src={world} alt="World" />
+                                        온라인
+                                    </div>
+                                    <div className="flex gap-x-1 items-center">
+                                        <img className="w-[18px] h-[16px]" src={user} alt="" />5 ~ 10명
+                                    </div>
+                                </div>
+                            </div>
+                            {/* class name */}
+                            <div className="w-full flex px-2">
+                                <p>클래스 이름 : Lorem ipsum dolor sit</p>
+                            </div>
+                            {/* class price */}
+                            <div className="flex justify-between px-2">
+                                <div className="flex gap-x-2 items-center">
+                                    <img src={star} alt="" />
+                                    찜하기
+                                </div>
+                                <div>100,000원</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/* title des */}
-            <div>
-              <p className='font-semibold text-[20px]'>어떤 클래스가 좋을지 모르겠다면? 많은 사람이 찾는 클래스부터 시작해보세요.</p>
-            </div>
-        </div>
-        {/* one-class */}
-        <div className='w-full flex mt-10 text-center gap-x-10 justify-center '>
-          <div className='w-full sm:w-[345px] h-full shadow-lg bg-white flex flex-col gap-y-[12px] rounded-2xl mb-3'>
-            {/* class img */}
-            <div className='w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden'>
-              <img src="" alt="" />이미지라오
-            </div>
-            {/* class genre */}
-            <div className='w-full flex justify-between px-2 items-center'>
-              <div className=' border border-black p-1 rounded-xl'>클래스 종류</div>
-              <div  className='flex gap-x-5'>
-                <div className=' flex gap-x-1 items-center'><img className='w-[16px] h-[18px]' src={world} alt="World" />온라인</div>
-                <div className='flex gap-x-1 items-center'><img className='w-[18px] h-[16px]' src={user} alt="" />5 ~ 10명</div>
-              </div>
-            </div>
-            {/* class name */}
-            <div className='w-full flex px-2'>
-              <p>클래스 이름 : Lorem ipsum dolor sit</p>
-            </div>
-            {/* class price */}
-            <div className='flex justify-between px-2'>
-              <div className='flex gap-x-2 items-center'><img src={star} alt="" />찜하기</div>
-              <div>100,000원</div>
-            </div>
-          </div>
-          <div className='w-full sm:w-[345px] h-full shadow-lg bg-white hidden sm:flex flex-col gap-y-[12px] rounded-2xl mb-3'>
-            {/* class img */}
-            <div className='w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden'>
-              <img src="" alt="" />이미지라오
-            </div>
-            {/* class genre */}
-            <div className='w-full flex justify-between px-2 items-center'>
-              <div className=' border border-black p-1 rounded-xl'>클래스 종류</div>
-              <div  className='flex gap-x-5'>
-                <div className=' flex gap-x-1 items-center'><img className='w-[16px] h-[18px]' src={world} alt="World" />온라인</div>
-                <div className='flex gap-x-1 items-center'><img className='w-[18px] h-[16px]' src={user} alt="" />5 ~ 10명</div>
-              </div>
-            </div>
-            {/* class name */}
-            <div className='w-full flex px-2'>
-              <p>클래스 이름 : Lorem ipsum dolor sit</p>
-            </div>
-            {/* class price */}
-            <div className='flex justify-between px-2'>
-              <div className='flex gap-x-2 items-center'><img src={star} alt="" />찜하기</div>
-              <div>100,000원</div>
-            </div>
-          </div>
-          <div className='w-full sm:w-[345px] h-full shadow-lg bg-white hidden lg:flex flex-col gap-y-[12px] rounded-2xl mb-3'>
-            {/* class img */}
-            <div className='w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden'>
-              <img src="" alt="" />이미지라오
-            </div>
-            {/* class genre */}
-            <div className='w-full flex justify-between px-2 items-center'>
-              <div className=' border border-black p-1 rounded-xl'>클래스 종류</div>
-              <div  className='flex gap-x-5'>
-                <div className=' flex gap-x-1 items-center'><img className='w-[16px] h-[18px]' src={world} alt="World" />온라인</div>
-                <div className='flex gap-x-1 items-center'><img className='w-[18px] h-[16px]' src={user} alt="" />5 ~ 10명</div>
-              </div>
-            </div>
-            {/* class name */}
-            <div className='w-full flex px-2'>
-              <p>클래스 이름 : Lorem ipsum dolor sit</p>
-            </div>
-            {/* class price */}
-            <div className='flex justify-between px-2'>
-              <div className='flex gap-x-2 items-center'><img src={star} alt="" />찜하기</div>
-              <div>100,000원</div>
-            </div>
-          </div>
-          <div className='w-full sm:w-[345px] h-full shadow-lg bg-white hidden xl:flex flex-col gap-y-[12px] rounded-2xl mb-3'>
-            {/* class img */}
-            <div className='w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden'>
-              <img src="" alt="" />이미지라오
-            </div>
-            {/* class genre */}
-            <div className='w-full flex justify-between px-2 items-center'>
-              <div className=' border border-black p-1 rounded-xl'>클래스 종류</div>
-              <div  className='flex gap-x-5'>
-                <div className=' flex gap-x-1 items-center'><img className='w-[16px] h-[18px]' src={world} alt="World" />온라인</div>
-                <div className='flex gap-x-1 items-center'><img className='w-[18px] h-[16px]' src={user} alt="" />5 ~ 10명</div>
-              </div>
-            </div>
-            {/* class name */}
-            <div className='w-full flex px-2'>
-              <p>클래스 이름 : Lorem ipsum dolor sit</p>
-            </div>
-            {/* class price */}
-            <div className='flex justify-between px-2'>
-              <div className='flex gap-x-2 items-center'><img src={star} alt="" />찜하기</div>
-              <div>100,000원</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-   </>
-  )
+        </>
+    );
 }
--- src/component/Qna.jsx
+++ src/component/Qna.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function Qna() {
-  return (
-    <div className='w-full flex justify-center py-5'>
-        <div className='w-full max-w-[70%] h-full flex flex-col gap-y-2'>
-            <h1 className='font-black text-[30px]'>자주 묻는 질문</h1>
-            <div className='flex flex-col border border-black rounded-2xl font-bold'>
-                <div className='flex justify-between border-b border-black px-4 py-2'>원데이클래스가 뭔가요?<button>∨</button></div>
-                <div className='flex justify-between border-b border-black px-4 py-2'>로그인하면 어떤혜택을 받을 수 있나요?<button>∨</button></div>
-                <div className='flex justify-between border-b border-black px-4 py-2'>온라인 클래스는 어떻게 진행되나요?<button>∨</button></div>
-                <div className='flex justify-between border-b border-black px-4 py-2'>혼자서도 참가할 수 있나요?<button>∨</button></div>
-                <div className='flex justify-between px-4 py-2'>결제는 어떻게 하나요?<button>∨</button></div>
+    return (
+        <div className="w-full flex justify-center py-5">
+            <div className="w-full max-w-[70%] h-full flex flex-col gap-y-2">
+                <h2 className="font-black text-[30px]">자주 묻는 질문</h2>
+                <div className="flex flex-col border border-black rounded-2xl font-bold">
+                    <details className="flex justify-between border-b border-black px-4 py-2">
+                        <summary>원데이클래스가 뭔가요?</summary>
+                        내용
+                    </details>
+                    <details className="flex justify-between border-b border-black px-4 py-2">
+                        <summary>로그인하면 어떤혜택을 받을 수 있나요?</summary>
+                    </details>
+                    <details className="flex justify-between border-b border-black px-4 py-2">
+                        <summary>온라인 클래스는 어떻게 진행되나요?</summary>
+                        내용
+                    </details>
+                    <details className="flex justify-between border-b border-black px-4 py-2">
+                        <summary>혼자서도 참가할 수 있나요?</summary>
+                        내용
+                    </details>
+                    <details className="flex justify-between px-4 py-2">
+                        <summary>결제는 어떻게 하나요?</summary>
+                        내용
+                    </details>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
