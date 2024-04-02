03f8e53fdc116419f93a60c704efeca6019725a0
ldh <ai04120@naver.com>
2024-04-02 16:50:33 +0900

    1

---
+++ src/component/ClassCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaGlobe, FaUsers, FaRegStar } from "react-icons/fa";
+
+export default function ClassCard(props) {
+    return (
+        <div className="w-full sm:w-[345px] h-full shadow-lg bg-white flex flex-col gap-y-[12px] rounded-2xl mb-3">
+            {/* class img */}
+            <div onClick={() => props.openModal("이미지Url")} className="w-full h-[30vh] bg-purple-200 rounded-2xl overflow-hidden">
+                <img src="" alt="" />
+                이미지라오
+            </div>
+            {/* class genre */}
+            <div className="w-full flex justify-between px-2 items-center">
+                <div className=" border border-black p-1 rounded-xl">클래스 종류</div>
+                <div className="flex gap-x-5">
+                    <div className=" flex gap-x-1 items-center">
+                        <FaGlobe />
+                        온라인
+                    </div>
+                    <div className="flex gap-x-1 items-center">
+                        <FaUsers />5 ~ 10명
+                    </div>
+                </div>
+            </div>
+            {/* class name */}
+            <div className="w-full flex px-2">
+                <p>클래스 이름 : Lorem ipsum dolor sit</p>
+            </div>
+            {/* class price */}
+            <div className="flex justify-between px-2">
+                <div className="flex gap-x-2 items-center">
+                    <FaRegStar />
+                    찜하기
+                </div>
+                <div>100,000원</div>
+            </div>
+        </div>
+    );
+}
--- src/component/Qna.jsx
+++ src/component/Qna.jsx
@@ -4,9 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const Detail = ({ summ, txt, border }) => {
     const [isOpen, setIsOpen] = useState(false);
-
     const toggleQna = () => setIsOpen(!isOpen);
-
     return (
         <div onClick={toggleQna} className="">
             <button className={`${border} px-5 py-[16px] border-[#ccc] w-full text-left flex justify-between items-center text-[20px] font-semibold`}>
@@ -29,7 +27,7 @@ export default function Qna() {
         <div className="w-full flex justify-center py-5 select-none">
             <div className="w-[70%] max-w-[1200px] h-full flex flex-col gap-y-2">
                 <h2 className="font-black text-[34px] mb-[20px]">자주 묻는 질문</h2>
-                <div className="flex flex-col border border-[#ccc] rounded-2xl">
+                <div className="flex flex-col border border-[#ccc] rounded-2xl overflow-hidden">
                     <Detail summ="원데이클래스가 뭔가요?" txt="내용" border="border-b" />
                     <Detail summ="로그인하면 어떤 혜택을 받을 수 있나요?" txt="내용" border="border-b" />
                     <Detail summ="온라인 클래스는 어떻게 진행되나요?" txt="내용" border="border-b" />
