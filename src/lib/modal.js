6ba05a0ed97da20031cdf7f67aed30d2369dfe59
parkjunhyun <jhyun970405@gmail.com>
2024-04-03 11:02:56 +0900

    모달 수정

--- src/lib/modal.js
+++ src/lib/modal.js
@@ -5,6 +5,7 @@ import location from "../img/location.png";
 import dollar from "../img/dollar.png";
 import clock from "../img/clock.png";
 import car from "../img/car.png";
+import star from "../img/star.png";
 
 export default function Modal({ imageUrl, onClose }) {
     return (
@@ -20,31 +21,48 @@ export default function Modal({ imageUrl, onClose }) {
                             <img src={imageUrl} alt="Class" />
                         </div>
                         {/* modal info */}
-                        <div className="w-[40%] h-[35vh] flex flex-col bg-slate-500 rounded-2xl p-2 gap-y-2">
+                        <div className="w-[40%] h-[37vh] flex flex-col bg-slate-500 rounded-2xl p-2 gap-y-2">
                             {/* name */}
                             <h2>클래스 이름 : lorem ipsum</h2>
                             {/* genre */}
-                            <p className="w-fit border text-[12px] border-black p-1 rounded-xl">클래스 종류</p>
+                            <p className="w-fit border text-[12px] border-black p-1 px-2 rounded-xl">클래스 종류</p>
                             {/* info */}
-                            <div className="flex flex-col gap-y-2">
-                                <p>
-                                    <img className="w-[18px] h-[21px]" src={location} alt="" />
+                            <div className="flex flex-col gap-y-[8px] pt-3">
+                                <p className="flex gap-x-3 items-center">
+                                    <img className="w-[17px] h-[21px] pl-[1px]" src={location} alt="" />
+                                    <p>내용 🐲</p>
                                 </p>
-                                <p>
-                                    <img className="w-[18px] h-[19px]" src={dollar} alt="" />
+                                <p className="flex gap-x-3 items-center">
+                                    <img className="w-[18px] h-[18px]" src={dollar} alt="" />
+                                    <p>내용 🐲</p>
                                 </p>
-                                <p>
+                                <p className="flex gap-x-3 items-center">
                                     <img className="w-[18px] h-[19px]" src={clock} alt="" />
+                                    <p>내용 🐲</p>
                                 </p>
-                                <p>
-                                    <img className="w-[18px] h-[19px]" src={world} alt="" />
+                                <p className="flex gap-x-3 items-center">
+                                    <img className="w-[18px] h-[18px]" src={world} alt="" />
+                                    <p>내용 🐲</p>
                                 </p>
-                                <p>
-                                    <img className="w-[18px] h-[19px]" src={user} alt="" />
+                                <p className="flex gap-x-3 items-center">
+                                    <img className="w-[18px] h-[15px]" src={user} alt="" />
+                                    <p>내용 🐲</p>
                                 </p>
-                                <p>
-                                    <img className="w-[18px] h-[19px]" src={car} alt="" />
+                                <p className="flex gap-x-3 items-center">
+                                    <img className="w-[18px] h-[17px]" src={car} alt="" />
+                                    <p>내용 🐲</p>
                                 </p>
+                            </div>
+                            {/* wishlist */}
+                            <div className="w-full flex justify-center pt-[7px] cursor-pointer">
+                                <div className="w-[90%] h-[40px] bg-white rounded-2xl flex justify-center gap-x-3">
+                                    <div className="flex items-center">
+                                        <img className="item w-[28px] h-[25px]" src={star} alt="start" />
+                                    </div>
+                                    <div className="flex items-center">
+                                        <p className="font-bold text-[17px]">관심 클래스</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
