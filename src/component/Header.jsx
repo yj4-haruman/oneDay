0c325b38a91188c5adf46cfeb1faf8bf478436be
ldh <ai04120@naver.com>
2024-04-03 15:06:54 +0900

    1

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { FaArrowRotateLeft } from "react-icons/fa6";
 
 export default function Header() {
-
   const [searchQuery, setSearchQuery] = useState("");
   const [classType, setClassType] = useState("");
   const [participants, setParticipants] = useState("");
@@ -47,51 +46,35 @@ export default function Header() {
               </div>
               {/* 메인 검색박스 */}
               <div className="w-full flex justify-center ">
-                <input 
-                className="border-2 w-[80%] h-[50px] pl-2 text-center placeholder-center rounded-2xl" 
-                type="text" 
-                placeholder="검색어 입력창" 
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                />
+                <input className="border-2 w-[80%] h-[50px] pl-2 text-center placeholder-center rounded-2xl" type="text" placeholder="검색어 입력창" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
               </div>
               {/* 세부 검색박스 */}
               <div className="w-full flex flex-wrap justify-center text-center gap-4 py-6">
                 <button className="w-fit">
-                  <select className=" bg-slate-300 rounded-2xl outline-none p-2" 
-                    value={classType}
-                    onChange={(e) => setClassType(e.target.value)}>
+                  <select className=" bg-slate-300 rounded-2xl outline-none p-2" value={classType} onChange={(e) => setClassType(e.target.value)}>
                     <option value="" disabled className="bg-white">
                       클래스 종류
-                    </option>
-                    <option value="group" className="bg-white">
-                      단체
                     </option>
                     <option value="drawing" className="bg-white">
                       드로잉
                     </option>
                     <option value="perfume" className="bg-white">
-                      향수
+                      음악
                     </option>
                     <option value="baking" className="bg-white">
-                      베이킹
+                      요리
                     </option>
                     <option value="sport" className="bg-white">
                       스포츠
                     </option>
                     <option value="ceramic" className="bg-white">
-                      도자기
-                    </option>
-                    <option value="date" className="bg-white">
-                      데이트
+                      체험
                     </option>
                     {/* 나머지 옵션들 추가 */}
                   </select>
                 </button>
                 <button className="w-fit bg-slate-300 rounded-2xl">
-                  <select className=" bg-slate-300 rounded-2xl outline-none p-2"
-                  value={participants}
-                  onChange={(e) => setParticipants(e.target.value)}>
+                  <select className=" bg-slate-300 rounded-2xl outline-none p-2" value={participants} onChange={(e) => setParticipants(e.target.value)}>
                     <option value="" disabled className="bg-white">
                       참가 인원
                     </option>
@@ -108,9 +91,7 @@ export default function Header() {
                   </select>
                 </button>
                 <button className="w-fit bg-slate-300 rounded-2xl">
-                      <select className=" bg-slate-300 rounded-2xl outline-none p-2"
-                      value={onlineOffline}
-                      onChange={(e) => setOnlineOffline(e.target.value)}>
+                  <select className=" bg-slate-300 rounded-2xl outline-none p-2" value={onlineOffline} onChange={(e) => setOnlineOffline(e.target.value)}>
                     <option value="" disabled className="bg-white">
                       온/오프라인
                     </option>
@@ -123,9 +104,7 @@ export default function Header() {
                   </select>
                 </button>
                 <button className="w-fit bg-slate-300 rounded-2xl">
-                      <select className=" bg-slate-300 rounded-2xl outline-none p-2"
-                      value={priceRange}
-                      onChange={(e) => setPriceRange(e.target.value)}>
+                  <select className=" bg-slate-300 rounded-2xl outline-none p-2" value={priceRange} onChange={(e) => setPriceRange(e.target.value)}>
                     <option value="" disabled className="bg-white">
                       가격
                     </option>
@@ -133,10 +112,10 @@ export default function Header() {
                       30,000원 ~
                     </option>
                     <option value="50000" className="bg-white">
-                     50,000원 ~
+                      50,000원 ~
                     </option>
                     <option value="100000" className="bg-white">
-                     100,000원 ~
+                      100,000원 ~
                     </option>
                     {/* 나머지 옵션들 추가 */}
                   </select>
--- src/component/New.jsx
+++ src/component/New.jsx
@@ -3,67 +3,58 @@ import Modal from "../lib/modal.js";
 import ClassCard from "./ClassCard.jsx";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { classListNew } from "../lib/classList.js";
 
 export default function Popular() {
-    const [showModal, setShowModal] = useState(false);
-    const [selectedImage, setSelectedImage] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
 
-    const openModal = (imageUrl) => {
-        setSelectedImage(imageUrl);
-        setShowModal(true);
-    };
+  const openModal = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setShowModal(true);
+  };
 
-    const closeModal = () => {
-        setShowModal(false);
-        setSelectedImage("");
-    };
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedImage("");
+  };
 
-    return (
-        <>
-            <div className="w-full flex px-5 my-16">
-                <div className="w-full h-full ">
-                    {/* title */}
-                    <div className="w-full max-w-[1600px] flex flex-col gap-y-2 mx-auto mb-12">
-                        {/* title */}
-                        <h2 className="font-black text-[35px]">새로운 클래스</h2>
-                        {/* title des */}
-                        <div className="font-semibold text-[20px]">어떤 클래스가 좋을지 모르겠다면? 많은 사람이 찾는 클래스부터 시작해보세요.</div>
-                    </div>
-                    {/* one-class */}
-                    <Swiper
-                        slidesPerView={1}
-                        loop={true}
-                        breakpoints={{
-                            768: {
-                                slidesPerView: 2,
-                            },
-                            1200: {
-                                slidesPerView: 3,
-                            },
-                            1500: {
-                                slidesPerView: 4,
-                            },
-                        }}
-                    >
-                        <SwiperSlide>
-                            <ClassCard openModal={openModal} />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <ClassCard openModal={openModal} />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <ClassCard openModal={openModal} />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <ClassCard openModal={openModal} />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <ClassCard openModal={openModal} />
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
-            </div>
-            {showModal && <Modal imageUrl={selectedImage} onClose={closeModal} />}
-        </>
-    );
+  return (
+    <>
+      <div className="w-full flex px-5 my-16">
+        <div className="w-full h-full ">
+          {/* title */}
+          <div className="w-full max-w-[1600px] flex flex-col gap-y-2 mx-auto mb-12">
+            {/* title */}
+            <h2 className="font-black text-[35px]">새로운 클래스</h2>
+            {/* title des */}
+            <div className="font-semibold text-[20px]">어떤 클래스가 좋을지 모르겠다면? 많은 사람이 찾는 클래스부터 시작해보세요.</div>
+          </div>
+          {/* one-class */}
+          <Swiper
+            slidesPerView={1}
+            loop={true}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1200: {
+                slidesPerView: 3,
+              },
+              1500: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {classListNew.map((item, index) => (
+              <SwiperSlide key={index}>
+                <ClassCard openModal={openModal} 종류={item.종류} 라인={item.라인} 인원={item.인원} 이름={item.이름} 가격={item.가격} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+      {showModal && <Modal imageUrl={selectedImage} onClose={closeModal} />}
+    </>
+  );
 }
