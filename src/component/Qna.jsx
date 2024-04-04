a4e3e56c9180d5cd54a4a27f6c9c182b3ac2bc6f
ldh <ai04120@naver.com>
2024-04-04 11:11:59 +0900

    1

--- src/component/Qna.jsx
+++ src/component/Qna.jsx
@@ -25,12 +25,12 @@ const Detail = ({ summ, txt, border }) => {
 export default function Qna() {
   return (
     <div className="w-full flex justify-center py-5 select-none">
-      <div className="w-[70%] max-w-[1200px] h-full flex flex-col gap-y-2">
+      <div className="w-[95%] max-w-[1100px] h-full flex flex-col gap-y-2">
         <h2 className="font-black text-[34px] mb-[20px]">자주 묻는 질문</h2>
-        <div className="flex flex-col border border-[#ccc] rounded-2xl overflow-hidden">
+        <div className="w-full max-w-[1100px] mx-auto flex flex-col border border-[#ccc] rounded-2xl overflow-hidden">
           <Detail summ="원데이클래스가 뭔가요?" txt="'원데이클래스'는 말 그대로 하루 몇시간 동안 가볍게 들을 수 있는 수업입니다. 바쁜 일상에서도 부담 없이 색다른 체험을 할 수 있습니다." border="border-b" />
           <Detail summ="로그인하면 어떤 혜택을 받을 수 있나요?" txt="관심가는 수업을 구독할 수 있게됩니다. 구독 후 알림톡을 신청하시면 수업일이 다가올 때마다 카카오톡으로 알림톡을 보내드립니다." border="border-b" />
-          <Detail summ="온라인 클래스는 어떻게 진행되나요?" txt="Zoom, Skype 등 화상회의 프로그램을 사용합니다. 재료가 필요한 클래스는 집으로 재료를 배송해드려요." border="border-b" />
+          <Detail summ="온라인 클래스는 어떻게 진행되나요?" txt="Zoom, Skype 등 화상회의 프로그램을 사용합니다." border="border-b" />
           <Detail summ="혼자서도 참가할 수 있나요?" txt="네. 모든 수업은 신청 인원에 상관없이 참가할 수 있습니다." border="border-b" />
           <Detail summ="결제는 어떻게 하나요?" txt="결제 기능은 구현 예정입니다." />
         </div>
