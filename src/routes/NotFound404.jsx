8cbfe30d4d1b96d291e9866984668cad18775cb9
parkjunhyun <jhyun970405@gmail.com>
2024-04-11 15:47:22 +0900

    1

--- src/routes/NotFound404.jsx
+++ src/routes/NotFound404.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function NotFound404() {
   return (
-   <div className='w-full flex justify-center'>
-        <div className='w-full max-w-[80%]'>
-            Not Found 404
+   <div className='w-full h-full flex justify-center'>
+        <div className='w-full max-w-[80%] h-screen flex justify-center items-center'>
+            <p className='font-black text-[80px]'>💥에러 에러 에에에엥💥</p>
         </div>
    </div>
   )
