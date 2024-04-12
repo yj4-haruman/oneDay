51534a9a4cec2881dee7866ca0458aa3eaaa5568
ldh <ai04120@naver.com>
2024-04-12 11:51:39 +0900

    1

--- src/lib/variants.js
+++ src/lib/variants.js
@@ -1,4 +1,4 @@
 export const sectVariants = {
-  start: { opacity: 0.5, y: "100px" },
-  end: { opacity: 1, y: 0, transition: { duration: 0.7 } },
+  start: { opacity: 0.5, y: "60px" },
+  end: { opacity: 1, y: 0, transition: { duration: 0.4 } },
 };
