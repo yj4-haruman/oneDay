97faab589e1700944c175166068841d381ed1892
ldh <ai04120@naver.com>
2024-04-12 14:02:37 +0900

    1

--- src/component/ClassCard.jsx
+++ src/component/ClassCard.jsx
@@ -4,13 +4,13 @@ import ClassCate from "./ClassCate";
 
 export default function ClassCard(props) {
   return (
-    <div className="w-full sm:w-[345px] h-full flex flex-col gap-y-[12px] rounded-2xl mb-3 mx-auto select-none cursor-pointer">
-      {/* class img */}
-      <div onClick={() => props.openModal(props.imageUrl, props.desc)} className="w-full h-[265px] rounded-2xl overflow-hidden classCard">
+    <div className="w-full sm:w-[345px] h-full flex flex-col rounded-2xl mb-3 mx-auto select-none">
+      {/* 사진 */}
+      <div onClick={() => props.openModal(props.imageUrl, props.desc)} className="w-full h-[265px] rounded-2xl overflow-hidden classCard cursor-pointer mb-3">
         <img className="w-full h-full bg-gray-200 object-cover" src={props.img} alt="클래스 이미지" />
       </div>
-      {/* class genre */}
-      <div className="w-full flex justify-between px-2 items-center">
+      {/* 정보 */}
+      <div className="w-full flex justify-between px-2 items-center mb-3">
         <ClassCate txt={props.type} />
         <div className="flex gap-x-5">
           <div className=" flex gap-x-1 items-center">
@@ -23,11 +23,11 @@ export default function ClassCard(props) {
           </div>
         </div>
       </div>
-      {/* class name */}
+      {/* 이름 */}
       <div className="w-full flex px-2 text-xl font-semibold">
         <p>{props.name}</p>
       </div>
-      {/* class price */}
+      {/* 가격 */}
       <div className="grid px-2 text-lg">
         <div className="justify-self-end">{props.price}원</div>
       </div>
